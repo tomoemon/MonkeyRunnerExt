2f3a6f7f79aab658e5f96ecd4af9767bbea4b1f1
tomoemon <bach48+github_tomoemon@gmail.com>
2014-10-20 22:16:34 +0900

    add support for taking snapshot as int array

--- src/com/android/monkeyrunner/MonkeyRunnerExt.java
+++ src/com/android/monkeyrunner/MonkeyRunnerExt.java
@@ -1,10 +1,28 @@
 package com.android.monkeyrunner;
 
+import java.awt.image.DataBufferByte;
 import java.io.IOException;
 
 public class MonkeyRunnerExt {
 
     public static void touchMove(MonkeyDevice device, int x, int y) throws IOException {
         device.getImpl().getManager().touchMove(x, y);
+    }
+
+    public static int[] takeSnapshotArray(MonkeyDevice device) {
+        return convertImageToArray(device.takeSnapshot());
+    }
+
+    public static int[] convertImageToArray(MonkeyImage image) {
+        // link: http://stackoverflow.com/questions/10247123/java-convert-bufferedimage-to-byte-without-writing-to-disk
+        return convertByteArrayToIntArray(((DataBufferByte)image.getImpl().getBufferedImage().getRaster().getDataBuffer()).getData());
+    }
+
+    private static int[] convertByteArrayToIntArray(byte[] array) {
+        int[] result = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            result[i] = (int)(array[i] & 0xFF);
+        }
+        return result;
     }
 }
